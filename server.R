2ae1cb574fd2612fdeffcfb6e74586c4257792bd
Yoyoyohanne <johannemargrethelund@hotmail.dk>
2024-11-05 11:00:01 +0100

    server

---
+++ server.R
@@ -0,0 +1,16 @@
+library("shiny")
+library("bslib")
+
+# Load needed functions
+source(file = "app_functions.R")
+
+server <- function(input, output) {
+  output$dna <- renderText({
+    gene_dna(length = input$n_bases,base_probs = c(input$prob_A,
+                                                   input$prob_T,
+                                                   input$prob_C,
+                                                   input$prob_G))
+  })
+  output$rna <- renderText({transcribe_dna(input$dna_seq)})
+  output$peptide <- renderText({translate_rna(input$rna_seq)})
+}
